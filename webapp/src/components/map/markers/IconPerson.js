b44321d5dcdfd16b603a9cc70fd6476fd7a4e801
manuel-arroyo <UO264184@uniovi.es>
2021-04-12 16:24:05 +0200

    Añadidos pngs de marcadores

--- webapp/src/components/map/markers/IconPerson.js
+++ webapp/src/components/map/markers/IconPerson.js
@@ -1,16 +1,13 @@
 import L from 'leaflet';
 
-
 const iconPerson = new L.Icon({
-    iconUrl: require('./friend-icon.svg'),
-    iconRetinaUrl: require('./friend-icon.svg'),
-    iconAnchor: null,
-    popupAnchor: null,
-    shadowUrl: null,
-    shadowSize: null,
-    shadowAnchor: null,
-    iconSize: new L.Point(60, 75),
-    className: 'leaflet-div-icon'
-});
+    iconUrl: require('./placeholder.png'),
+    shadowUrl: require('./placeholder.png'),
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76]// point from which the popup should open relative to the iconAnchor
+})
 
 export { iconPerson };

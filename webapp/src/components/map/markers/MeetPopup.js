0ee9bc7af9dce6dfe9e4c90c923b7d4bfb495251
UO271612 <UO271612@uniovi.es>
2021-04-29 00:42:52 +0200

    Convertida MeetPopup a clase

--- webapp/src/components/map/markers/MeetPopup.js
+++ webapp/src/components/map/markers/MeetPopup.js
@@ -15,82 +15,166 @@ import { getMeet } from "../../../api/api";
 
 import Button from '@material-ui/core/Button';
 
+class MeetPopup extends React.Component {
 
-
-
-function checkInMeet(meet, loggedWebId) {
-
-    if (!meet.attendances.includes(loggedWebId)) {
-        assist(meet._id, loggedWebId)
-    } else {
-        removeAttendance(meet._id, loggedWebId)
-
+    constructor({ webId, fullName, imageSrc, meet, loggedWebId }) {
+        super();
+        this.webId = webId;
+        this.fullName = fullName;
+        this.imageSrc = imageSrc;
+        this.meet = meet;
+        this.loggedWebId = loggedWebId;
     }
 
-}
+    checkInMeet() {
 
-
-function checkMeetText(meet, loggedWebId) {
-    if (meet.attendances.includes(loggedWebId)) {
-        meet.attendances.push(loggedWebId)
-        return "No asistir"
-    } else {
-        const index = meet.attendances.indexOf(loggedWebId);
-        if (index > -1) {
-            meet.attendances.splice(index, 1);
+        if (!this.meet.attendances.includes(this.loggedWebId)) {
+            assist(this.meet._id, this.loggedWebId)
+        } else {
+            removeAttendance(this.meet._id, this.loggedWebId)
+    
         }
-        return "Asistir"
+    
+    }
+    
+    
+    checkMeetText() {
+        if (this.meet.attendances.includes(this.loggedWebId)) {
+            this.meet.attendances.push(this.loggedWebId)
+            return "No asistir"
+        } else {
+            const index = this.meet.attendances.indexOf(this.loggedWebId);
+            if (index > -1) {
+                this.meet.attendances.splice(index, 1);
+            }
+            return "Asistir"
+        }
+    
+    }
+    
+    showAttendantsList() {
+        var promise = getMeet(this.meet._id);
+    
+        promise.then((result) => {
+            console.log("OBTUVE MEET");
+            this.meet = result;
+            console.log(result);
+        });
     }
 
-}
-
-function showAttendantsList(meet) {
-    var promise = getMeet(meet._id);
-
-    promise.then((result) => {
-        console.log("OBTUVE MEET");
-        meet = result;
-        console.log(result);
-    });
-}
-
-//Meets
-const Meet = ({ webId, fullName, imageSrc, meet, loggedWebId }) =>(
-
-    <div>
-        <strong>Meet Creado por</strong>
-        <ListItem className={styles.friend} button component={Link} to={toProfile(webId)} clickable>
-            <ListItemIcon>
-                <Image className={styles.avatar} fullName={fullName} src={imageSrc} />
-            </ListItemIcon>
-            <div style={{ marginLeft: 10 }}>
-                <ListItemText
-                    primary={fullName}
-                />
-            </div>
-
-        </ListItem>
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2em' }}>
-                <ListItemText
-                    primary={"Fecha: " + meet.date}
-                />
-                <ListItemText
-                    primary={"Hora: " + meet.time}
-                />
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap' }}>
-                <div style={{ marginRight: '1.5em', padding: '0.25em' }}>
-                    <Button color="primary" variant="contained" onClick={() => { checkInMeet(meet, loggedWebId).bind(meet) }}>  {checkMeetText(meet, loggedWebId)}   </Button>
+    render() {
+        return (
+            <div>
+            <strong>Meet Creado por</strong>
+            <ListItem className={styles.friend} button component={Link} to={toProfile(this.webId)} clickable>
+                <ListItemIcon>
+                    <Image className={styles.avatar} fullName={this.fullName} src={this.imageSrc} />
+                </ListItemIcon>
+                <div style={{ marginLeft: 10 }}>
+                    <ListItemText
+                        primary={this.fullName}
+                    />
                 </div>
-                <div style={{ padding: '0.25em' }}>
-                    <Button color="primary" variant="contained" onClick={() => { showAttendantsList(meet) }}>Lista de Usuarios</Button>
+
+            </ListItem>
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2em' }}>
+                    <ListItemText
+                        primary={"Fecha: " + this.meet.date}
+                    />
+                    <ListItemText
+                        primary={"Hora: " + this.meet.time}
+                    />
+                </div>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap' }}>
+                    <div style={{ marginRight: '1.5em', padding: '0.25em' }}>
+                        <Button color="primary" variant="contained" onClick={() => { this.checkInMeet() }}>  {this.checkMeetText()}   </Button>
+                    </div>
+                    <div style={{ padding: '0.25em' }}>
+                        <Button color="primary" variant="contained" onClick={() => { this.showAttendantsList() }}>Lista de Usuarios</Button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+        );
+    }
+
+}
+
+
+// function checkInMeet(meet, loggedWebId) {
+
+//     if (!meet.attendances.includes(loggedWebId)) {
+//         assist(meet._id, loggedWebId)
+//     } else {
+//         removeAttendance(meet._id, loggedWebId)
+
+//     }
+
+// }
+
+
+// function checkMeetText(meet, loggedWebId) {
+//     if (meet.attendances.includes(loggedWebId)) {
+//         meet.attendances.push(loggedWebId)
+//         return "No asistir"
+//     } else {
+//         const index = meet.attendances.indexOf(loggedWebId);
+//         if (index > -1) {
+//             meet.attendances.splice(index, 1);
+//         }
+//         return "Asistir"
+//     }
+
+// }
+
+// function showAttendantsList(meet) {
+//     var promise = getMeet(meet._id);
+
+//     promise.then((result) => {
+//         console.log("OBTUVE MEET");
+//         meet = result;
+//         console.log(result);
+//     });
+// }
+
+// //Meets
+// const Meet = ({ webId, fullName, imageSrc, meet, loggedWebId }) =>(
+
+//     <div>
+//         <strong>Meet Creado por</strong>
+//         <ListItem className={styles.friend} button component={Link} to={toProfile(webId)} clickable>
+//             <ListItemIcon>
+//                 <Image className={styles.avatar} fullName={fullName} src={imageSrc} />
+//             </ListItemIcon>
+//             <div style={{ marginLeft: 10 }}>
+//                 <ListItemText
+//                     primary={fullName}
+//                 />
+//             </div>
+
+//         </ListItem>
+//         <div style={{ display: 'flex', flexDirection: 'column' }}>
+//             <div style={{ display: 'flex', flexDirection: 'row', marginBottom: '2em' }}>
+//                 <ListItemText
+//                     primary={"Fecha: " + meet.date}
+//                 />
+//                 <ListItemText
+//                     primary={"Hora: " + meet.time}
+//                 />
+//             </div>
+//             <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', flexWrap: 'wrap' }}>
+//                 <div style={{ marginRight: '1.5em', padding: '0.25em' }}>
+//                     <Button color="primary" variant="contained" onClick={() => { checkInMeet(meet, loggedWebId).bind(meet) }}>  {checkMeetText(meet, loggedWebId)}   </Button>
+//                 </div>
+//                 <div style={{ padding: '0.25em' }}>
+//                     <Button color="primary" variant="contained" onClick={() => { showAttendantsList(meet) }}>Lista de Usuarios</Button>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+// )
 
 
 
-export default Meet
+export default MeetPopup

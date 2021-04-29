5963fac6ce20908cc32a63eef0a664f6b5d922f5
uo269450 <UO269450@uniovi.es>
2021-04-29 16:23:41 +0200

    Refactor

--- webapp/src/components/map/MeetLocationMarkers.jsx
+++ webapp/src/components/map/MeetLocationMarkers.jsx
@@ -7,6 +7,11 @@ import Meet from "./markers/MeetPopupManager"
 
 
 
+
+
+
+
+
 export default class MeetLocationMarkers extends Component {
     constructor(props) {
         super(props);
@@ -15,10 +20,9 @@ export default class MeetLocationMarkers extends Component {
             locs: [],
             updated: false
         }
+
+
     }
-
-
-
 
     async componentDidMount() {
         this.updateMeets()
@@ -32,8 +36,8 @@ export default class MeetLocationMarkers extends Component {
     async timeUpdateMeets() {
         const interval = setInterval(() => {
             this.updateMeets()
-
-        }, 20000);
+            console.log("Updated Meets")
+        }, 2000);
         return () => clearInterval(interval);
     }
 
@@ -55,12 +59,15 @@ export default class MeetLocationMarkers extends Component {
 
                 <LayerGroup >
                     {this.state.locs.map((loc) => {
-                        return loc.creator != this.state.webId ? <Marker position={[loc.location[0], loc.location[1]]} icon={iconMeet}>
+                        return loc.creator != this.state.webId ?
 
-                            <Popup>
-                                <Meet meet={loc} loggedWebId={this.state.webId} />
-                            </Popup>
-                        </Marker> :
+                            <Marker position={[loc.location[0], loc.location[1]]} icon={iconMeet}>
+
+                                <Popup>
+                                    <Meet meet={loc} loggedWebId={this.state.webId} />
+                                </Popup>
+                            </Marker>
+                            :
                             <Marker position={[loc.location[0], loc.location[1]]} icon={iconOwnMeet}>
 
                                 <Popup>

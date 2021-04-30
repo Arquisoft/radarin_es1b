03f6dec65448ef86f890cfb8b0ecccff9d58cd51
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-04-30 17:01:13 +0200

    completar test profile

--- webapp/src/App.test.js
+++ webapp/src/App.test.js
@@ -1,7 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import {shallow} from 'enzyme';
+import NavBar from './components/NavBar';
 test('renders learn react link', () => {
   var result=render(<App />);
   expect(result).toBeTruthy();
+   
+});
+describe('App', function () {
+  it('should link to profile page of given WebID', () => {
+    var result=shallow(<App />);
+    const nav=result.find(NavBar);
+    expect(nav).toBeTruthy();
+  });
+
 });
---
+++ webapp/src/components/profile/__test__/index.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {shallow, render} from 'enzyme';
+import Index from '../Index';
+import ProfileContainer from '../Container';
+import useProfile from "../useProfile";
+
+const literal = value => ({
+  toString: () => value
+});
+describe('Profile container', () => {
+
+    it('should not render profile while pending', () => {
+      const result = shallow(<Index pending/>);
+      expect(result.find(ProfileContainer)).toEqual({});
+    });
+  
+    
+    
+      });

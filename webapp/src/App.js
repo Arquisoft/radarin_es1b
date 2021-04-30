a198b325334a68bf3ea834a3b36f45859a96f940
AlbertoFreije <freicartobe@gmail.com>
2021-04-30 13:06:25 +0200

    prueba nueva alternativa offline/online

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -55,10 +55,35 @@ class App extends React.Component {
         })
         }
 
+        idleLogout() {
+          var t;
+          window.onload = resetTimer;
+          window.onmousemove = resetTimer;
+          window.onmousedown = resetTimer;  // catches touchscreen presses as well      
+          window.ontouchstart = resetTimer; // catches touchscreen swipes as well 
+          window.onclick = resetTimer;      // catches touchpad clicks as well
+          window.onkeypress = resetTimer;   
+          window.addEventListener('scroll', resetTimer, true); // improved; see comments
+      
+          function yourFunction() {
+              // your function for too long inactivity goes here
+              // e.g. window.location.href = 'logout.php';
+              console.log("CERRANDO SESION")
+          }
+      
+          function resetTimer() {
+              clearTimeout(t);
+              t = setTimeout(yourFunction, 10000);  // time is in milliseconds
+              console.log("TIEMPO " + t)
+          }
+      }
+      
+
       componentDidMount(){
 
         this.fetchData();
         this.fetchBan();
+        this.idleLogout();
 
       }
 

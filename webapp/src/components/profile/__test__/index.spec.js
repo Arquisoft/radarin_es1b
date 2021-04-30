8fafdce335bd399a3c91b6ad7a0e099243d69a14
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-04-30 17:31:47 +0200

    completar test profile

--- webapp/src/components/profile/__test__/index.spec.js
+++ webapp/src/components/profile/__test__/index.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow, render} from 'enzyme';
-import Index from '../Index';
+import Index from '../index';
 import ProfileContainer from '../Container';
 import useProfile from "../useProfile";
 

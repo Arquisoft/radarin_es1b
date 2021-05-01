a936b9e8a3298b866bf46240c7d1b514cc5cfe05
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-05-01 16:53:23 +0200

    intentosearch

--- webapp/src/components/friends/_test_/SearchFriends.spec.js
+++ webapp/src/components/friends/_test_/SearchFriends.spec.js
@@ -1,38 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
-
-import SearchFriends from '../Friends';
+import {shallow, mount} from 'enzyme';
+import SearchFriends from '../SearchFriends';
 import { Button } from '@material-ui/core';
 import Friend from "../../friendList/friend";
 
+import InfiniteScroll from "react-infinite-scroll-component";
 
-
-let container;
+import List from "@material-ui/core/List";
 
 const literal = value => ({
   toString: () => value
 });
 
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
+describe('Search ', function () {
+  it('shows an empty list', () => {
+    const result = shallow(<SearchFriends webId={literal('https://uo225211.solidcommunity.net//profile/card#me')}/>);
+    const friends = result.find(List).find(Friend);
+    expect(friends.length>=0).toBeTruthy();
+  });
+
+  it('shows an InfiniteScroll', () => {
+    const result = shallow(<SearchFriends webId={literal('https://uo225211.solidcommunity.net//profile/card#me')}/>);
+    const scrooll = result.find(InfiniteScroll);
+    expect(scrooll).toBeTruthy();
+  });
+
+
+  it('shows the given maximum of friends initially', () => {
+    const result = shallow(<SearchFriends webId={literal('https://uo225211.solidcommunity.net//profile/card#me')}/>);
+    const boton = result.find(List).find(Button);
+    expect(boton).toBeTruthy();
+    
+  });
+  
+  it('shows the given maximum of friends initially', () => {
+    const result = shallow(<SearchFriends />);
+    const boton = result.find(List).find(Button);
+    const query= result.prop('querySuccess');
+    //expect(query).toBe(false);
+    const searchname=result.state('searchName');
+    expect(searchname).toBe("");
+    console.log(result.debug());
+  });
+  
 });
 
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-it('can render and update a search', () => {
-  // Prueba la primer renderización y componentDidMount
-  act(() => {    ReactDOM.render(<SearchFriends webId={literal('https://uo225211.solidcommunity.net/')}/>, container);  }); 
-  const button = container.querySelector('button');
-  const label = container.querySelector('querySuccess');
-  //expect(container.state).toBe('searchName: ""');
-  expect(label).toBe(null);
-  // Prueba la segunda renderización y componentDidUpdate
-  act(() => {    button.dispatchEvent(new MouseEvent('click', {bubbles: true}));  }); 
-  expect(label).toBe(null);
-  //expect(contador).toBe(true);
-});

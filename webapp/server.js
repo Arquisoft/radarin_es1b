bd091c1df719510515332889e0c24519f1a078dd
uo269450 <UO269450@uniovi.es>
2021-05-01 11:25:19 +0200

    Solución bug Prueba Opcion 2

--- webapp/server.js
+++ webapp/server.js
@@ -1,4 +1,10 @@
 var express = require('express')
+
+app.get('*' , (req, res) => {
+    res.sendFile(path.join(__dirname,'build','index.html'));
+    });
+
+
 var app = express()
 var port =  process.env.PORT || 3000
 app.use(express.static('build'))

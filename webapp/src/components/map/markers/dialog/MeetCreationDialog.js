28e7f2b1215022d6aaba95995aba3100371e9d01
uo269450 <UO269450@uniovi.es>
2021-05-02 20:31:25 +0200

    Arreglado MeetCreation Dialog

--- webapp/src/components/map/markers/dialog/MeetCreationDialog.js
+++ webapp/src/components/map/markers/dialog/MeetCreationDialog.js
@@ -6,7 +6,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import 'date-fns';  
+import 'date-fns';
 import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import {
@@ -23,20 +23,24 @@ export default function MeetCreationDialog(props) {
   const [open, setOpen] = React.useState(props.open);
   const [name, setName] = React.useState("");
   const [selectedDate, setSelectedDate] = React.useState(Date.now());
-  const [handleCancel, setHandleCancel] = React.useState(props.handleCancel);
-  const [handleCreate, setHandleCreate] = React.useState(props.handleCreate);
 
   const handleClose = () => {
     setOpen(false);
-    handleCancel();
   };
 
+
+  const handleCancel=()=>{
+    setOpen(false);
+    props.handleCancel();
+  }
+
   const handleNameChange = (name) => {
-    setName(name);
-};
+
+    setName(name.target.value);
+  };
 
   const handleDateChange = (date) => {
-      setSelectedDate(date);
+    setSelectedDate(date);
   };
 
   const handleInput = () => {
@@ -44,17 +48,22 @@ export default function MeetCreationDialog(props) {
 
     if (name.trim().length == 0) {
       check = false;
-      toast.error("Introduce un nombre para el Meet.", { position : "top-center"});
+      toast.error("Introduce un nombre para el Meet.", { position: "top-center" });
     }
 
     let now = Date.now();
     if (selectedDate < now) {
       check = false;
-      toast.error("Introduce una fecha y hora posteriores a la actual.", { position : "top-center"});
+      toast.error("Introduce una fecha y hora posteriores a la actual.", { position: "top-center" });
     }
 
-    if (check)
-      handleCreate();
+
+
+    if (check) {
+      
+      props.handleCreate(name,selectedDate);
+      handleClose()
+    }
   }
 
   return (
@@ -76,37 +85,37 @@ export default function MeetCreationDialog(props) {
             value={name}
             onChange={handleNameChange}
           />
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Grid container justify="space-around">
-                    <KeyboardDatePicker
-                    required
-                    margin="normal"
-                    id="date-picker-dialog"
-                    label="Fecha"
-                    format="dd/MM/yyyy"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change date',
-                    }}
-                    />
-                    <KeyboardTimePicker
-                    required
-                    ampm={false}
-                    margin="normal"
-                    id="time-picker"
-                    label="Hora"
-                    value={selectedDate}
-                    onChange={handleDateChange}
-                    KeyboardButtonProps={{
-                        'aria-label': 'change time',
-                    }}
-                    />
-                </Grid>
-            </MuiPickersUtilsProvider>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid container justify="space-around">
+              <KeyboardDatePicker
+                required
+                margin="normal"
+                id="date-picker-dialog"
+                label="Fecha"
+                format="dd/MM/yyyy"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change date',
+                }}
+              />
+              <KeyboardTimePicker
+                required
+                ampm={false}
+                margin="normal"
+                id="time-picker"
+                label="Hora"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                  'aria-label': 'change time',
+                }}
+              />
+            </Grid>
+          </MuiPickersUtilsProvider>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
+          <Button onClick={handleCancel} color="primary">
             Cancelar
           </Button>
           <Button onClick={handleInput} color="primary">

9efc06656e070ce27fd2b1fb8ae4c41b719c8aa5
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-05-03 01:44:31 +0200

    e2e cambios

--- webapp/e2e/steps/register-form.steps.js
+++ webapp/e2e/steps/register-form.steps.js
@@ -79,7 +79,7 @@ defineFeature(feature, test => {
 
     then('An error message should be shown in the screen', async () => {
     });
-    expect(await page.url()).toBe("https://radarines1bwebapp.herokuapp.com/");
+    expect(page.url()).toBe("https://radarines1bwebapp.herokuapp.com/");
   });
 });
 

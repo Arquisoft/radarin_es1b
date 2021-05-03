c824aaf8465ec6e1ad88556d29ad242841dc805c
AlbertoFreije <freicartobe@gmail.com>
2021-05-03 21:35:02 +0200

    baneado anterior

--- webapp/src/routes/StartPageBaneado.jsx
+++ webapp/src/routes/StartPageBaneado.jsx
@@ -10,7 +10,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-
 const useStyles = makeStyles(() => ({
   media: {
     objectFit: 'contain'
@@ -27,8 +26,8 @@ const Start = () => {
           direction="column">
       <Card>
         <CardContent>
-          <Typography gutterBottom variant="h2" component="h1" style = {{fontWeight: 800}}>
-           USTED HA SIDO BANEADO
+          <Typography gutterBottom variant="h2" component="h1">
+           Usted esta baneado
           </Typography>
         </CardContent>
       </Card>
@@ -38,9 +37,8 @@ const Start = () => {
           direction="column">
       <Card>
         <CardContent>
-           <img src="/img/alert.png" alt="logo"  style={{marginBottom: "1em"}}/>
-          <Typography component="p" style = {{fontWeight: 800}}>
-            Ha sido baneado por un comportamiento indebido 
+          <Typography component="p">
+            se ha portado muy mal
           </Typography>
         </CardContent>
       </Card>

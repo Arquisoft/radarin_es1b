6efb26cddbb4e44e29697085a673f0f3d758faa6
uo225211 <71693558+uo225211@users.noreply.github.com>
2021-05-04 12:09:57 +0200

    Algunos test de restappp

--- restapi/tests/api.test.js
+++ restapi/tests/api.test.js
@@ -48,4 +48,55 @@ describe('user ', () => {
         expect(response.body.webId).toBe(email);
 
     });
+
+    it('show de list of users empty', async () => {
+        const response = await request(app).post('/api/users/list')
+        expect(response.statusCode).toBe(404);
+
+    });
+
+    it('/users/remove', async () => {
+        username = 'Pablo'
+        email = 'pablo@uniovi.es'        
+        admin = 'false';
+        status = 'offline';
+        const response = await request(app).post('/api/users/add').send({nombre: username,webId: email})
+        expect(response.statusCode).toBe(200);
+        const response2 = await request(app).post('/api/users/remove').send({webId: email})
+        expect(response2.statusCode).toBe(200);
+    });
+    
+    it('/users/lastTime/update', async () => {
+        username = 'Pablo'
+        email = 'pablo@uniovi.es'        
+        admin = 'false';
+        status = 'offline';
+        const response = await request(app).post('/api/users/add').send({nombre: username,webId: email})
+        expect(response.statusCode).toBe(200);
+        date = new Date();
+        const response2 = await request(app).post('/api/users/lastTime/update').send({webId: email, time: date})
+        expect(response2.statusCode).toBe(200);
+        //console.log(response2.body)
+        const date2=response2.body.time;
+        expect(new Date(date2)).toStrictEqual(date)
+        expect(response2.body.webId).toBe(email);
+    });
+
+    
+
+    it('change status', async () => {
+        username = 'Pablo'
+        email = 'pablo@uniovi.es'        
+        admin = 'false';
+        status = 'offline';
+        const response = await request(app).post('/api/users/add').send({nombre: username,webId: email})
+        expect(response.statusCode).toBe(200);
+        status = 'online';
+        const response2 = await request(app).post('/api/users/status/update').send({webId: email, status: status})
+        expect(response2.statusCode).toBe(200);
+        console.log(response2.body)
+        //expect(response2.body.status).toBe(status);
+        expect(response2.body.webId).toBe(email);
+    });
+
 });
